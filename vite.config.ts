497d41f39deece7e6f29102d2134843e885efb52
Nuszpl Botond <nuszplbotond@gmail.com>
2026-03-19 13:23:05 +0100

    fix: update base path in Vite configuration for local deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dashboard-performance-test/',
+  base: './',
   plugins: [react()],
 })
